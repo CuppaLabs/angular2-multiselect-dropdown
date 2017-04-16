c76a0ba5c56bea3f2390ffa6f0234ec9ade8d03f
pradeep <solomon.terli@gmail.com>
2017-04-17 00:14:14 +0530

    demos and docs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,12 +24,7 @@ export class AppComponent implements OnInit {
                           {"id":2,"itemName":"Singapore"},
                           {"id":3,"itemName":"Australia"},
                           {"id":4,"itemName":"Canada"},
-                          {"id":5,"itemName":"South Korea"},
-                          {"id":6,"itemName":"Germany"},
-                          {"id":7,"itemName":"France"},
-                          {"id":8,"itemName":"Russia"},
-                          {"id":9,"itemName":"Italy"},
-                          {"id":10,"itemName":"Sweden"}
+                          {"id":5,"itemName":"South Korea"}
                         ];
     this.dropdownList2 = [
                           {"id":1,"itemName":"India"},
