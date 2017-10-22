3a0db02ab0ceabdb8ddbd2bfaf16905ddda180e4
Pradeep Terli <solomon.terli@gmail.com>
2017-10-22 16:22:29 +0530

    groupby bug fixes  #66

--- src/app/angular2-multiselect-dropdown/multiselect.component.ts
+++ src/app/angular2-multiselect-dropdown/multiselect.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, NgModule, OnChanges, ViewEncapsulation,forwardRef, Input, Output, EventEmitter, ElementRef, AfterViewInit, Pipe, PipeTransform } from '@angular/core';
+import { Component, OnInit, NgModule, OnChanges, ViewEncapsulation, forwardRef, Input, Output, EventEmitter, ElementRef, AfterViewInit, Pipe, PipeTransform } from '@angular/core';
 import { FormsModule, NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
-import { CommonModule }       from '@angular/common';
-import { ListItem , MyException} from './multiselect.model';
+import { CommonModule } from '@angular/common';
+import { ListItem, MyException } from './multiselect.model';
 import { DropdownSettings } from './multiselect.interface';
 import { ClickOutsideDirective } from './clickOutside';
 import { ListFilterPipe } from './list-filter';
@@ -17,21 +17,21 @@ const noop = () => {
 };
 
 @Component({
-    selector:'angular2-multiselect',
+    selector: 'angular2-multiselect',
     templateUrl: './multiselect.component.html',
-    host: { '[class]' : 'defaultSettings.classes'  },
-    styleUrls:['./multiselect.component.scss'],
+    host: { '[class]': 'defaultSettings.classes' },
+    styleUrls: ['./multiselect.component.scss'],
     providers: [DROPDOWN_CONTROL_VALUE_ACCESSOR]
 })
 
 export class AngularMultiSelect implements OnInit, ControlValueAccessor {
 
-    @Input() 
-    data: Array<ListItem>;
-    
     @Input()
-    settings:DropdownSettings;
-    
+    data: Array<ListItem>;
+
+    @Input()
+    settings: DropdownSettings;
+
     @Output('onSelect')
     onSelect: EventEmitter<ListItem> = new EventEmitter<ListItem>();
 
@@ -47,96 +47,100 @@ export class AngularMultiSelect implements OnInit, ControlValueAccessor {
     public selectedItems: Array<ListItem>;
     public isActive: boolean = false;
     public isSelectAll: boolean = false;
+    public groupedData: Array<ListItem>;
     filter: ListItem = new ListItem();
-    defaultSettings:DropdownSettings = {
+    defaultSettings: DropdownSettings = {
         singleSelection: false,
-        text:'Select',
+        text: 'Select',
         enableCheckAll: true,
         selectAllText: 'Select All',
         unSelectAllText: 'UnSelect All',
         enableSearchFilter: false,
         maxHeight: 300,
         badgeShowLimit: 999999999999,
-        classes:'',
+        classes: '',
         disabled: false,
         searchPlaceholderText: 'Search'
     }
-    constructor(){
+    constructor() {
 
     }
-    ngOnInit(){
-         this.settings = Object.assign(this.defaultSettings, this.settings);
+    ngOnInit() {
+        this.settings = Object.assign(this.defaultSettings, this.settings);
+        if (this.settings.groupBy) {
+            this.groupedData = this.transformData(this.data, this.settings.groupBy);
+        }
     }
-    ngDoCheck(){
-        if(this.selectedItems){
-            if(this.selectedItems.length == 0 || this.data.length == 0 || this.selectedItems.length < this.data.length){
+    ngDoCheck() {
+        if (this.selectedItems) {
+            if (this.selectedItems.length == 0 || this.data.length == 0 || this.selectedItems.length < this.data.length) {
                 this.isSelectAll = false;
             }
         }
     }
-    onItemClick(item: ListItem, index: number, evt: Event){
-                if(this.settings.disabled){
-                    return false;
-                }
-        
-                let found = this.isSelected(item);
-                let limit = this.selectedItems.length < this.settings.limitSelection ? true : false;
+    onItemClick(item: ListItem, index: number, evt: Event) {
+        if (this.settings.disabled) {
+            return false;
+        }
 
-                if(!found){
-                    if(this.settings.limitSelection){
-                        if(limit){
-                            this.addSelected(item);
-                            this.onSelect.emit(item);
-                        } 
-                    }
-                    else{
-                        this.addSelected(item);
-                        this.onSelect.emit(item);
-                    }
-                    
+        let found = this.isSelected(item);
+        let limit = this.selectedItems.length < this.settings.limitSelection ? true : false;
+
+        if (!found) {
+            if (this.settings.limitSelection) {
+                if (limit) {
+                    this.addSelected(item);
+                    this.onSelect.emit(item);
                 }
-                else{
-                this.removeSelected(item);
-                    this.onDeSelect.emit(item);
-                }
-                if(this.isSelectAll || this.data.length > this.selectedItems.length){
-                    this.isSelectAll = false;
-                }
-                if(this.data.length == this.selectedItems.length){
-                    this.isSelectAll = true;
-                }    
+            }
+            else {
+                this.addSelected(item);
+                this.onSelect.emit(item);
+            }
+
+        }
+        else {
+            this.removeSelected(item);
+            this.onDeSelect.emit(item);
+        }
+        if (this.isSelectAll || this.data.length > this.selectedItems.length) {
+            this.isSelectAll = false;
+        }
+        if (this.data.length == this.selectedItems.length) {
+            this.isSelectAll = true;
+        }
     }
     private onTouchedCallback: () => void = noop;
     private onChangeCallback: (_: any) => void = noop;
 
     writeValue(value: any) {
         if (value !== undefined && value !== null) {
-            if(this.settings.singleSelection){
-                try{
-                    
-                    if(value.length > 1){
+            if (this.settings.singleSelection) {
+                try {
+
+                    if (value.length > 1) {
                         this.selectedItems = [value[0]];
                         throw new MyException(404, { "msg": "Single Selection Mode, Selected Items cannot have more than one item." });
                     }
-                    else{
+                    else {
                         this.selectedItems = value;
                     }
                 }
-                catch(e){
+                catch (e) {
                     console.error(e.body.msg);
                 }
-                
+
             }
-            else{
-                if(this.settings.limitSelection){
-                            this.selectedItems = value.splice(0,this.settings.limitSelection);
-                        }
-                 else{
-                        this.selectedItems = value;
-                    }
-                if(this.selectedItems.length === this.data.length && this.data.length > 0){
-                            this.isSelectAll = true;
-                        }
+            else {
+                if (this.settings.limitSelection) {
+                    this.selectedItems = value.splice(0, this.settings.limitSelection);
+                }
+                else {
+                    this.selectedItems = value;
+                }
+                if (this.selectedItems.length === this.data.length && this.data.length > 0) {
+                    this.isSelectAll = true;
+                }
             }
         } else {
             this.selectedItems = [];
@@ -152,65 +156,80 @@ export class AngularMultiSelect implements OnInit, ControlValueAccessor {
     registerOnTouched(fn: any) {
         this.onTouchedCallback = fn;
     }
-    trackByFn(index: number, item: ListItem){
+    trackByFn(index: number, item: ListItem) {
         return item.id;
     }
-    isSelected(clickedItem:ListItem){
+    isSelected(clickedItem: ListItem) {
         let found = false;
         this.selectedItems && this.selectedItems.forEach(item => {
-           if(clickedItem.id === item.id){
-               found = true;
-           }
+            if (clickedItem.id === item.id) {
+                found = true;
+            }
         });
         return found;
     }
-    addSelected(item: ListItem){
-        if(this.settings.singleSelection){
+    addSelected(item: ListItem) {
+        if (this.settings.singleSelection) {
             this.selectedItems = [];
             this.selectedItems.push(item);
         }
         else
             this.selectedItems.push(item);
-            this.onChangeCallback(this.selectedItems);
-    }
-    removeSelected(clickedItem: ListItem){
-        this.selectedItems && this.selectedItems.forEach(item => {
-           if(clickedItem.id === item.id){
-               this.selectedItems.splice(this.selectedItems.indexOf(item),1);
-           }
-        });    
         this.onChangeCallback(this.selectedItems);
     }
-    toggleDropdown(evt: any){
-        if(this.settings.disabled){
+    removeSelected(clickedItem: ListItem) {
+        this.selectedItems && this.selectedItems.forEach(item => {
+            if (clickedItem.id === item.id) {
+                this.selectedItems.splice(this.selectedItems.indexOf(item), 1);
+            }
+        });
+        this.onChangeCallback(this.selectedItems);
+    }
+    toggleDropdown(evt: any) {
+        if (this.settings.disabled) {
             return false;
         }
         this.isActive = !this.isActive;
         evt.preventDefault();
     }
-    closeDropdown(){
+    closeDropdown() {
         this.isActive = false;
     }
-    toggleSelectAll(){
-        if(!this.isSelectAll){
+    toggleSelectAll() {
+        if (!this.isSelectAll) {
             this.selectedItems = [];
             this.selectedItems = this.data.slice();
             this.isSelectAll = true;
             this.onChangeCallback(this.selectedItems);
             this.onSelectAll.emit(this.selectedItems);
         }
-        else{
+        else {
             this.selectedItems = [];
             this.isSelectAll = false;
             this.onChangeCallback(this.selectedItems);
             this.onDeSelectAll.emit(this.selectedItems);
-        }     
+        }
+    }
+    transformData(arr: Array<ListItem>, field: any):Array<ListItem> {
+        const groupedObj:any = arr.reduce((prev:any, cur:any) => {
+            if (!prev[cur[field]]) {
+                prev[cur[field]] = [cur];
+            } else {
+                prev[cur[field]].push(cur);
+            }
+            return prev;
+        }, {});
+        const tempArr:any = [];
+        Object.keys(groupedObj).map(function (x) {
+            tempArr.push({ key: x, value: groupedObj[x] });
+        });
+        return tempArr;
     }
 }
 
 @NgModule({
-  imports:      [ CommonModule,FormsModule ],
-  declarations: [AngularMultiSelect, ClickOutsideDirective, ListFilterPipe, groupByPipe],
-  exports:      [AngularMultiSelect, ClickOutsideDirective, ListFilterPipe, groupByPipe]
+    imports: [CommonModule, FormsModule],
+    declarations: [AngularMultiSelect, ClickOutsideDirective, ListFilterPipe, groupByPipe],
+    exports: [AngularMultiSelect, ClickOutsideDirective, ListFilterPipe, groupByPipe]
 })
 export class AngularMultiSelectModule { }
