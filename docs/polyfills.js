c7d8311456b361a57f0e14842500cb188a5d81f4
Pradeep Terli <solomon.terli@gmail.com>
2019-06-04 00:45:35 +0530

    example docs update

--- docs/polyfills.js
+++ docs/polyfills.js
@@ -5473,7 +5473,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\pterli\Documents\GitHub\angular2-multiselect-dropdown\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! /Users/solomon301/Documents/GitHub/angular2-multiselect-dropdown/src/polyfills.ts */"./src/polyfills.ts");
 
 
 /***/ })
